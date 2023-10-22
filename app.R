92624721c8edd59a42080c195a34efdb6988b4bb
toylyol <105601415+toylyol@users.noreply.github.com>
2023-10-21 20:44:21 -0400

    finalizing dataset filter logic

--- app.R
+++ app.R
@@ -1,14 +1,15 @@
 
 # Load packages
 
-packages <- c("shiny", "dplyr", "stringr", "magrittr")
+packages <- c("shiny", "dplyr", "stringr", "ggplot2", "reactable")
 
 invisible(lapply(packages, library, character.only = TRUE))
 
 
 # Load data
 
-raw_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-12-21/starbucks.csv')
+raw_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-12-21/starbucks.csv',
+                            show_col_types = FALSE)
 
 
 # Clean up data for my purposes
@@ -19,66 +20,92 @@ starbucks <- raw_data |>
                               str_detect(product_name,
                                          "Lemonade|Smoothie|Hot Chocolate|Fibre Powder|Refresher|Apple Spice|Crème Frappuccino Blended") ~ "Other",
                               TRUE ~ "Coffee"
-                               ),
-         .after = product_name
+  ), 
+  .before = product_name
   ) |> 
   mutate(product_name = str_to_title(product_name)) |> 
   mutate(size = str_to_title(size))
+
+plot(raw_data$calories, raw_data$sugar_g)
+
 
 
 # Create the app itself
 
 ui <- fluidPage(
   
-  selectInput(inputId = "select_cat", 
+  selectInput("select_cat", 
               label = "Category", 
               choices = sort(unique(starbucks$category)),
-              multiple = TRUE),
-  selectInput(inputId = "select_bev",
-              label = "Drinks",
-              choices = NULL,
-              multiple = TRUE),
-  selectInput(inputId = "select_size",
-              label = "Size",
-              choices = NULL,
-              multiple = TRUE),
-  tableOutput("table")
+              multiple = TRUE,
+              selectize = TRUE
+  ),
+  uiOutput("select_bev"), # name child
+  uiOutput("select_size"), # name grandchild
+  plotOutput("chart"),
+  reactableOutput("table")
   
 )
 
 server <- function(input, output, session) {
   
-  category <- reactive({
-    filter(starbucks, category %in% input$select_cat)
-  })
-  
-  observeEvent(category(), {
-    choices <- category()$product_name |> 
-      unique() |> 
+  output$select_bev <- renderUI({
+    drinks <- starbucks %>%
+      filter(category %in% input$select_cat) %>%
+      pull(product_name) %>%
+      unique() %>%
       sort()
-    updateSelectInput(inputId = "select_bev", choices = choices) 
+    
+    selectInput("select_bev", 
+                label = "Drink",
+                choices = drinks,
+                multiple = TRUE
+    )
   })
   
-  drinks <- reactive({
-    req(input$select_bev)
-    filter(category(), product_name %in% input$select_bev)
-  })
-  
-  observeEvent(drinks(), {
-    choices <- drinks()$size |> 
-      unique() |> 
+  output$select_size <- renderUI({
+    sizes <- starbucks %>%
+      filter(product_name %in% input$select_bev) %>%
+      pull(size) %>%
+      unique() %>%
       sort()
-    updateSelectInput(inputId = "select_size", choices = choices)
+    
+    selectInput("select_size", "Size",
+                choices = sizes,
+                multiple = TRUE
+    )
   })
   
-  output$table <- renderTable({
-    req(input$select_size)
-    drinks() %>% 
-      filter(size %in% input$select_size)
+  dataset <- reactive({
+    
+    if( !is.null(input$select_cat) & !is.null(input$select_bev) & !is.null(input$select_size) ){
+      starbucks |>
+        filter(category %in% input$select_cat)|> 
+        filter(product_name %in% input$select_bev) |> 
+        filter(size %in% input$select_size)  } else 
+          if(!is.null(input$select_cat) & !is.null(input$select_bev) & is.null(input$select_size)){
+            starbucks |>
+              filter(category %in% input$select_cat)|> 
+              filter(product_name %in% input$select_bev)} else
+                if(!is.null(input$select_cat) & is.null(input$select_bev) & is.null(input$select_size)){
+                  starbucks |> filter(category %in% input$select_cat)} else {
+                    starbucks
+                  }
+    
+  })
+  
+  output$chart <- renderPlot({
+    plot(dataset()$calories, dataset()$sugar_g)
+    })
+  
+  output$table <- renderReactable({
+    reactable(dataset())
   })
   
 }
 
 shinyApp(ui, server)
 
-# See Chapter 10.1.2 at https://mastering-shiny.org/action-dynamic.html#hierarchical-select
+# https://www.linkedin.com/pulse/shiny-app-r-integrating-filter-multiple-dynamic-conditions-lee-rock/
+# https://community.rstudio.com/t/reactive-filter-using-multiple-inputs-in-shiny/28974
+# https://www.davidsolito.com/post/conditional-drop-down-in-shiny/
--- playing_in_sandbox.R
+++ playing_in_sandbox.R
@@ -1,5 +1,5 @@
 
-###################################
+#**********************************
 # See Chapter 10 of Mastering Shiny ----
 
 library(shiny)
@@ -22,8 +22,8 @@ server <- function(input, output, session) {
 shinyApp(ui, server)
 
 
-##########################################
-# See first attempt at parent-child inputs ----
+#******************************************
+# See Hierarchical Select in Chapter 10.1.2 ----
 
 
 # Load packages
@@ -57,15 +57,146 @@ starbucks <- raw_data |>
 
 ui <- fluidPage(
   
-  selectInput("select_cat", 
+  selectInput(inputId = "select_cat", 
               label = "Category", 
               choices = sort(unique(starbucks$category)),
-              multiple = TRUE,
-              selectize = TRUE
-  ),
-  uiOutput("select_bev"), # name child
-  uiOutput("select_size"), # name grandchild
+              multiple = TRUE),
+  selectInput(inputId = "select_bev",
+              label = "Drinks",
+              choices = NULL,
+              multiple = TRUE),
+  selectInput(inputId = "select_size",
+              label = "Size",
+              choices = NULL,
+              multiple = TRUE),
   tableOutput("table")
+  
+)
+
+server <- function(input, output, session) {
+  
+  category <- reactive({
+    filter(starbucks, category %in% input$select_cat)
+  })
+  
+  observeEvent(category(), {
+    choices <- category()$product_name |> 
+      unique() |> 
+      sort()
+    updateSelectInput(inputId = "select_bev", choices = choices) 
+  })
+  
+  drinks <- reactive({
+    req(input$select_bev)
+    filter(category(), product_name %in% input$select_bev)
+  })
+  
+  observeEvent(drinks(), {
+    choices <- drinks()$size |> 
+      unique() |> 
+      sort()
+    updateSelectInput(inputId = "select_size", choices = choices)
+  })
+  
+  output$table <- renderTable({
+    req(input$select_size)
+    drinks() |>  
+      filter(size %in% input$select_size)
+  })
+  
+}
+
+shinyApp(ui, server)
+
+
+#********************
+# Chapter 3 Observers ----
+
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting"),
+  span(
+    "Example",
+    div(style = "display:inline-block;",
+        title = "A tooltip",
+        icon("info-circle")))
+)
+
+server <- function(input, output, session) {
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  
+  output$greeting <- renderText(string())
+  observeEvent(input$name, {
+    message("Greeting performed")  # The message prints to console.
+  })
+}
+
+shinyApp(ui, server)
+
+
+#******************************
+# Failed attempt to add tabsets ----
+
+
+# Load packages
+
+packages <- c("shiny", "dplyr", "stringr", "ggplot2", "reactable")
+
+invisible(lapply(packages, library, character.only = TRUE))
+
+
+# Load data
+
+raw_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-12-21/starbucks.csv',
+                            show_col_types = FALSE)
+
+
+# Clean up data for my purposes
+
+starbucks <- raw_data |>
+  mutate(category = case_when(str_detect(product_name, 
+                                         "Chai|Tea|tea") ~ "Tea",
+                              str_detect(product_name,
+                                         "Lemonade|Smoothie|Hot Chocolate|Fibre Powder|Refresher|Apple Spice|Crème Frappuccino Blended") ~ "Other",
+                              TRUE ~ "Coffee"
+  ), 
+  .before = product_name
+  ) |> 
+  mutate(product_name = str_to_title(product_name)) |> 
+  mutate(size = str_to_title(size))
+
+
+# Create the app itself
+
+ui <- fluidPage(
+  
+  titlePanel("Starbucks Nutrition"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      selectInput("select_cat", 
+                  label = "Category", 
+                  choices = sort(unique(starbucks$category)),
+                  multiple = TRUE),
+      
+      uiOutput("select_bev"), # name child
+      
+      uiOutput("select_bev") # name child
+      
+    ),
+    
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Chart", plotOutput("chart")),
+                  tabPanel("Table", reactableOutput("table"))
+      )
+    )
+    
+  )
   
 )
 
@@ -100,27 +231,30 @@ server <- function(input, output, session) {
   
   dataset <- reactive({
     
-    # req(input$select_cat)
-    # 
-    # starbucks |> 
-    #   filter(category %in% input$select_cat)
-    
-    if( !is.null(input$select_cat) ){
+    if( !is.null(input$select_cat) & !is.null(input$select_bev) & !is.null(input$select_size) ){
       starbucks |>
-        filter(category %in% input$select_cat)} else {
-          starbucks
-        }
+        filter(category %in% input$select_cat)|> 
+        filter(product_name %in% input$select_bev) |> 
+        filter(size %in% input$select_size)  } else 
+          if(!is.null(input$select_cat) & !is.null(input$select_bev) & is.null(input$select_size)){
+            starbucks |>
+              filter(category %in% input$select_cat)|> 
+              filter(product_name %in% input$select_bev)} else
+                if(!is.null(input$select_cat) & is.null(input$select_bev) & is.null(input$select_size)){
+                  starbucks |> filter(category %in% input$select_cat)} else {
+                    starbucks
+                  }
     
   })
   
-  output$table <- renderTable({
-    dataset()
+  ouput$chart <- renderPlot({
+    plot(dataset()$calories, dataset()$sugar_g)
+  })
+  
+  output$table <- renderReactable({
+    reactable(dataset())
   })
   
 }
 
 shinyApp(ui, server)
-
-# https://www.linkedin.com/pulse/shiny-app-r-integrating-filter-multiple-dynamic-conditions-lee-rock/
-# https://community.rstudio.com/t/reactive-filter-using-multiple-inputs-in-shiny/28974
-# https://www.davidsolito.com/post/conditional-drop-down-in-shiny/
